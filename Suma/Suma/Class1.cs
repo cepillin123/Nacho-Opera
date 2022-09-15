f104d6745e4bfa024cefec0431b950ce6e506298
cepillin123 <joan.herrera481@pascualbravo.edu.co>
2022-09-15 10:55:23 -0500

    Carpeta suma modificada

--- Suma/Suma/Class1.cs
+++ Suma/Suma/Class1.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Suma
+namespace suma
 {
-    public class Class1
+    public class suma
     {
+        /// <summary>
+        /// Metodo suma
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns></returns>
+        public static long Suma(long i, long j)
+        {
+            return i + j;
+        }
     }
+
 }
