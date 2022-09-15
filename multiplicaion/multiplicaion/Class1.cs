d6d61826f716a67ecfd6b9f7769ed2145ddf4df3
cepillin123 <joan.herrera481@pascualbravo.edu.co>
2022-09-15 10:47:42 -0500

    Agregada carpeta multiplicacion

---
+++ multiplicaion/multiplicaion/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplicaion
+{
+    public class multiplicacion
+    {
+        /// <summary>
+        /// Metodo multiplicar 
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns></returns>
+        public static long Multi( long i, long j)
+        {
+            return (i * j);
+
+        }
+    }
+}
