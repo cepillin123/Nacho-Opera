59d8a71dfc72e2e3a0adcee45e08497385190b7d
cepillin123 <joan.herrera481@pascualbravo.edu.co>
2022-09-15 11:17:46 -0500

    Carpeta resta agregada

---
+++ Multiplicacion/Multiplicacion/Resta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resta
+{
+    public class resta
+    {
+
+        /// <summary>
+        /// Metodo restar 
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns></returns>
+        public static long Resta(long i, long j)
+        {
+            return (i - j);
+
+        }
+    }
+}
