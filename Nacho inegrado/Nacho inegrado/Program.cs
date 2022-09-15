823cfa26e7f29d4a7325034a24a2da54379f4e38
cepillin123 <joan.herrera481@pascualbravo.edu.co>
2022-09-15 11:32:53 -0500

    nacho integrado modificado

--- Nacho inegrado/Nacho inegrado/Program.cs
+++ Nacho inegrado/Nacho inegrado/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using suma;
+using resta;
+using multiplicaion;
 
 namespace Nacho_inegrado
 {
@@ -10,6 +13,18 @@ namespace Nacho_inegrado
     {
         static void Main(string[] args)
         {
+            long variable;
+
+            //Suma
+            variable = suma.suma.Suma(2, 9);
+            Console.WriteLine(variable);
+            //resta
+            variable = resta.resta.Resta(2, 9);
+            Console.WriteLine(variable);
+            //multiplicacion
+            variable = multiplicacion.Multi(2, 9);
+            Console.WriteLine(variable);
+
         }
     }
 }
